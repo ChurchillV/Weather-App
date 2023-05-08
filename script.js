1b1ef67275c7f6b70fca0e2e40b52eb28a92c439
ChurchillV <vinchurch70@gmail.com>
2023-05-08 16:28:09 +0000

    Icons change based on city weather data

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const submit_button = document.getElementById('submit');
+const submit_button = document.querySelector('#submit');
 const temp = document.querySelector('.temp');
-const name = document.getElementById('location');
+const name = document.querySelector('#location');
 const wind_speed = document.querySelector('.wind-speed');
 const humidity = document.querySelector('.humidity');
+const description = document.querySelector('.desc');
 const apiKey = '25f8704ce87a636134414384d067d31c';
+const icon = document.querySelector('.weather-icon');
 
 submit_button.addEventListener('click', function() {
     const cityName = document.querySelector('#name_value').value;
-    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
@@ -15,8 +17,11 @@ submit_button.addEventListener('click', function() {
         console.log(cityName);
         name.innerHTML = data['name'];
         temp.innerHTML = `${(data['main']['temp'] - 273).toFixed(1)}°C`;
-        wind_speed.innerHTML = `${data['wind']['speed']}km/h`;
-        humidity.innerHTML = `${data['main']['humidity']}%`;        
+        wind_speed.innerHTML = `${data.wind.speed}km/h`;
+        humidity.innerHTML = `${data.main.humidity}%`; 
+        description.innerHTML = `${data.weather[0].main}`;
+        icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        icon.style.width = '200px';
     })
     .catch(err => console.log(err))
 })
