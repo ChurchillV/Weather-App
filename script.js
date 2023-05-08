c5588a203083c9bbd9293f330a4958add6e25f46
ChurchillV <vinchurch70@gmail.com>
2023-05-08 20:22:48 +0000

    Changed a variable name (name -> location)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const submit_button = document.querySelector('#submit');
 const temp = document.querySelector('.temp');
-const name = document.querySelector('#location');
+const location = document.querySelector('.location');
 const wind_speed = document.querySelector('.wind-speed');
 const humidity = document.querySelector('.humidity');
 const description = document.querySelector('.desc');
@@ -16,7 +16,7 @@ submit_button.addEventListener('click', function() {
     .then(data => {
         console.log(data);
         console.log(cityName);
-        name.innerHTML = data['name'];
+        location.innerHTML = data['name'];
         temp.innerHTML = `${(data['main']['temp'] - 273).toFixed(1)}°C`;
         wind_speed.innerHTML = `${data.wind.speed}km/h`;
         humidity.innerHTML = `${data.main.humidity}%`; 
